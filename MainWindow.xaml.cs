c7d76fd9b1827c1ad7285a430e43103f815ded8a
Thane5 <13683581+Thane5@users.noreply.github.com>
2024-10-20 18:01:28 +0200

    Add checks for path and camera to update button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using MediaDevices;
 using Microsoft.Win32;
@@ -13,6 +15,8 @@ public partial class MainWindow : Window
     public static MainWindow mainWindow;
     public static DeviceLink deviceLink = new();
     OpenFolderDialog folderDialog = new();
+
+    private MediaDevice? camera;
     String path = "";
     
         
@@ -22,24 +26,59 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         mainWindow = this;
-        CheckCamera();
+
+        LoadSettings();
+        UpdateCopyButton();
     }
 
-    private void CheckCamera()
+    private bool CameraOkay()
     {
-        var camera = deviceLink.FindCamera();
+        camera = deviceLink.FindCamera();
         string statusDisplay;
         if (camera!= null)
         {
             statusDisplay = camera.Manufacturer + " " + camera.FriendlyName;
-            cameraStatus.Text = statusDisplay;
-            cameraStatus.Foreground = Brushes.LimeGreen;
+            CameraStatus.Text = statusDisplay;
+            CameraStatus.Foreground = Brushes.LimeGreen;
+            return true;    
+        }
+
+        statusDisplay = "No Camera Found";
+        CameraStatus.Text = statusDisplay;
+        CameraStatus.Foreground = Brushes.Orange;
+        return false;
+    }
+
+    private bool PathOkay()
+    {
+        if (Directory.Exists(PathDisplay.Text))
+        {
+            path = PathDisplay.Text;
+            return true;
+        }
+        return false;
+    }
+    
+
+    private void UpdateCopyButton()
+    {
+        if (CameraOkay() && PathOkay())
+        {
+            BtnCopy.IsEnabled = true;
         }
         else
         {
-            statusDisplay = "No Camera Found";
-            cameraStatus.Text = statusDisplay;
-            cameraStatus.Foreground = Brushes.Orange;
+            BtnCopy.IsEnabled = false;
+        }
+            
+    }
+
+    private void LoadSettings()
+    {
+        // Todo: load settings from file
+        if (true)
+        {
+            PathDisplay.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
         }
     }
     
@@ -52,15 +91,27 @@ public partial class MainWindow : Window
 
     private void OnCLick_BtnBrowse(object sender, RoutedEventArgs e)
     {
+        folderDialog.DefaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        // Todo: load the last path and set the InitialDirectory here
+        //folderDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        if (PathOkay())
+        {
+            folderDialog.InitialDirectory = path;
+        }
+        
         if (folderDialog.ShowDialog() != true) return;
         var folderName = folderDialog.FolderName;
-        // Do something with the result
         Console.WriteLine("got a file " + folderName);
         PathDisplay.Text = folderName;
     }
 
     private void OnCLick_BtnCheck(object sender, RoutedEventArgs e)
     {
-        CheckCamera();
+        UpdateCopyButton();
+    }
+
+    private void OnChange_PathDisplay(object sender, TextChangedEventArgs e)
+    {
+        UpdateCopyButton();
     }
 }
