0caa6fdf18f468dd22d5e7509d2cd9762f459058
Thane5 <13683581+Thane5@users.noreply.github.com>
2024-10-27 17:34:40 +0100

    swich to pygubu GUI

--- main.py
+++ main.py
@@ -1,65 +1,42 @@
 import os
+import pathlib
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
+import pygubu
 import sv_ttk
 import pywinstyles, sys
 
 import DeviceLink
 
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "MainWindow.ui"
+
 class QuickCameraImporterApp:
+    def __init__(self, master=None):
+        # 1: Create a builder and setup resources path (if you have images)
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
 
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Quick Camera Importer")
-        self.root.geometry("640x300")
+        # 2: Load a ui file
+        builder.add_from_file(PROJECT_UI)
 
-        # Apply a theme (optional, adjust as needed)
-        self.style = ttk.Style()
-        #self.style.theme_use("clam")  # Try "alt", "default", "classic" or any theme available on your system
+        # 3: Create the mainwindow
+        self.mainwindow = builder.get_object('mainwindow', master)
+
+        # 4: Connect callbacks
+        builder.connect_callbacks(self)
+
+        # 5: Apply Windows 11 theme
         sv_ttk.use_dark_theme()
+        apply_theme_to_titlebar(self.mainwindow)
 
-        self.create_widgets()
-        self.update_ui()  # Initial update to set the camera status
+        # update text
+        self.label_camerastatus = builder.get_variable('var_camerastatus')  # Gets the label's variable
+        self.label_camerastatus.set('new text')
 
-        # Add a close event handler to ensure proper cleanup
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-    def on_closing(self):
-        self.root.quit()
-        self.root.destroy()
-
-    def create_widgets(self):
-        # Camera Panel
-        camera_frame = ttk.Frame(self.root, padding=(10, 5))
-        camera_frame.pack(pady=(20, 0), fill='x')
-
-        camera_label = ttk.Label(camera_frame, text="Selected Camera:")
-        camera_label.pack(side="left", padx=(10, 5))
-
-        self.camera_status = ttk.Label(camera_frame, text="No Camera Found", foreground="red",
-                                       font=("Helvetica", 12, "bold"))
-        self.camera_status.pack(side="left", expand=True, fill="x")
-
-        self.check_camera_button = ttk.Button(camera_frame, text="Check Connection", command=self.update_ui)
-        self.check_camera_button.pack(side="right", padx=10)
-
-        # Path Panel
-        path_frame = ttk.Frame(self.root, padding=(10, 5))
-        path_frame.pack(pady=(20, 0), fill='x')
-
-        path_label = ttk.Label(path_frame, text="Copy pictures to:")
-        path_label.pack(side="left", padx=(10, 5))
-
-        self.path_entry = ttk.Entry(path_frame)
-        self.path_entry.insert(0, "C:/tmp")
-        self.path_entry.pack(side="left", fill="x", expand=True)
-
-        self.browse_button = ttk.Button(path_frame, text="Browse", command=self.browse_folder)
-        self.browse_button.pack(side="right", padx=10)
-
-        # Copy Now Button
-        self.copy_button = ttk.Button(self.root, text="Copy Now", command=self.copy_now, state="disabled", width=20)
-        self.copy_button.pack(pady=20)
+    def run(self):
+        self.mainwindow.mainloop()
 
 
     def update_ui(self):
@@ -84,13 +61,8 @@ class QuickCameraImporterApp:
 
 
     def camera_okay(self):
-        print("i am camera_okaycamera_okay")
-
         if DeviceLink.find_camera() is None:
-            print("camera_okaycamera_okay returns False")
             return False
-
-        print("camera_okaycamera_okay returns True")
         return True
 
 
@@ -113,12 +85,16 @@ class QuickCameraImporterApp:
             self.path_entry.delete(0, tk.END)
             self.path_entry.insert(0, selected_path)
 
+
     def copy_now(self):
         self.update_ui()
         selected_path = self.path_entry.get()
         print("selected_path:", selected_path)
         DeviceLink.copy_files(DeviceLink.currentDevice, selected_path)
 
+
+def open_main_window():
+    pass
 
 def apply_theme_to_titlebar(window):
     version = sys.getwindowsversion()
@@ -131,10 +107,8 @@ def apply_theme_to_titlebar(window):
 
 if __name__ == "__main__":
     try:
-        root = tk.Tk()
-        app = QuickCameraImporterApp(root)
-        apply_theme_to_titlebar(root)
-        root.mainloop()
+        app = QuickCameraImporterApp()
+        app.run()
     except KeyboardInterrupt:
         pass
 
